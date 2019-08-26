c04319100ba94acc495d9e3058a444f06e8a5f6a
Ajay <ajay@froiden.com>
2019-08-26 15:05:11 +0530

    Feat: added web to routes

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['namespace' => 'Froiden\Envato\Controllers'], function () {
+Route::group(['namespace' => 'Froiden\Envato\Controllers', 'middleware' => 'web'], function () {
 
     Route::get('verify-purchase', ['uses' => 'PurchaseVerificationController@verifyPurchase'])->name('verify-purchase');
     Route::post('purchase-verified', ['uses' => 'PurchaseVerificationController@purchaseVerified'])->name('purchase-verified');
@@ -8,7 +8,7 @@ Route::group(['namespace' => 'Froiden\Envato\Controllers'], function () {
     Route::get('update-database', ['uses' => 'PurchaseVerificationController@updateDatabase'])->name('update-database');
 
     // update script version
-    Route::group(['as' => 'admin.',['middleware' => 'auth','web']], function () {
+    Route::group(['as' => 'admin.'], function () {
         Route::get('update-version/update', ['as' => 'updateVersion.update', 'uses' => 'UpdateScriptVersionController@update']);
         Route::get('update-version/download', ['as' => 'updateVersion.download', 'uses' => 'UpdateScriptVersionController@download']);
         Route::get('update-version/downloadPercent', ['as' => 'updateVersion.downloadPercent', 'uses' => 'UpdateScriptVersionController@downloadPercent']);
