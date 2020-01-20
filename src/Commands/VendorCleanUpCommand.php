f912b734e922c9dddaf093652b285640b5ea1a7a
Ajay Kumar Choudhary <ajay@froiden.com>
2020-01-20 12:32:48 +0530

    Upgrade* removed from vendor clean up

--- src/Commands/VendorCleanUpCommand.php
+++ src/Commands/VendorCleanUpCommand.php
@@ -24,7 +24,6 @@ class VendorCleanUpCommand extends Command
         'changelog*',
         'contributing*',
         'upgrading*',
-        'upgrade*',
         '.idea',
         '.vagrant',
         'readme*',
