e7364df7aae24bf3e2a904b734269b7867032232
Ajay Kumar Choudhary <ajay@froiden.com>
2020-01-28 17:16:27 +0530

    Update AppBoot.php

--- src/Traits/AppBoot.php
+++ src/Traits/AppBoot.php
@@ -126,7 +126,10 @@ trait AppBoot
 
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
-
+            // Object Object Error for verification
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $server_output = curl_exec($ch);
             $response = json_decode($server_output, true);
