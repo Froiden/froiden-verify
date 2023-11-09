eeecdeb5086e5e482b07537ea39a9841df4b1dfb
Ajay Kumar Choudhary <ajay@froiden.com>
2023-11-09 22:39:46 +0530

    Update NewVersionModule.php

--- src/Commands/NewVersionModule.php
+++ src/Commands/NewVersionModule.php
@@ -52,7 +52,7 @@ class NewVersionModule extends Command
 
         $path = $this->createVersionZip($version);
 
-        $this->comment("\n" . $this->module . '-' . $version.' and '. $this->module . '-auto-' . $version.' is Ready to distribute');
+        $this->comment("\n" . $this->module . '-' . $version . ' and ' . $this->module . '-auto-' . $version . ' is Ready to distribute');
 
         $this->comment("\n" . 'run ./upload.sh to upload to froiden server');
 
@@ -61,17 +61,17 @@ class NewVersionModule extends Command
     private function createVersionZip($version)
     {
 
-        $folder = $this->product.'-'.strtolower($this->module) . '-module-' . $version;
+        $folder = $this->product . '-' . strtolower($this->module) . '-module-' . $version;
         $versionFolder = '../versions/';
-        $path = $versionFolder . $folder . '/'.$this->module;
-        $local = '../' . $this->product.'/Modules/'.$this->module . '/';
+        $path = $versionFolder . $folder . '/' . $this->module;
+        $local = '../' . $this->product . '/Modules/' . $this->module . '/';
         $this->comment($local);
         $this->comment("\n\n" . '------Creating Versions------');
         $this->info(' Removing Old ' . $folder . ' folder to create the new');
-        echo exec('rm -rf '.$versionFolder . $folder);
-        echo exec('rm -rf '.$versionFolder . $folder.'.zip');
+        echo exec('rm -rf ' . $versionFolder . $folder);
+        echo exec('rm -rf ' . $versionFolder . $folder . '.zip');
 
-        $this->info(' Creating the directory ' . $folder . '/'.$this->module);
+        $this->info(' Creating the directory ' . $folder . '/' . $this->module);
         echo exec('mkdir -p ' . $path);
 
         if (strtolower($this->module) == 'universalbundle') {
@@ -103,15 +103,15 @@ class NewVersionModule extends Command
         $this->info(' Removing old version.txt file');
         echo exec('rm ' . $path . '/version.txt');
 
-        $this->info(' Copying '.$version.' version to know the version to version.txt file');
+        $this->info(' Copying ' . $version . ' version to know the version to version.txt file');
         echo exec('echo ' . $version . '>> ' . $path . '/version.txt');
 
-        $this->info(' Moving '.$this->module.'/documentation to separate folder');
-        echo exec('mv ' . $path . '/Documentation ' . $path.'/../documentation/');
-        $this->info('cd '.$versionFolder . $folder.'; zip -r ' . $this->module . '.zip '.$this->module);
+        $this->info(' Moving ' . $this->module . '/documentation to separate folder');
+        echo exec('mv ' . $path . '/Documentation ' . $path . '/../documentation/');
+        $this->info('cd ' . $versionFolder . $folder . '; zip -r ' . $this->module . '.zip ' . $this->module);
 
         // Creating of module
-        echo exec('cd '.$versionFolder . $folder.'; zip -r ' . $this->module . '.zip '.$this->module);
+        echo exec('cd ' . $versionFolder . $folder . '; zip -r ' . $this->module . '.zip ' . $this->module);
 
 
         echo exec('rm -rf ' . $path);
@@ -125,26 +125,26 @@ class NewVersionModule extends Command
 
     private function copyModulesToUniversalBundleModules()
     {
-        $modules = File::directories(base_path().'/Modules');
-        $universalBundleModulePath = base_path().'/Modules/UniversalBundle/Modules';
+        $modules = File::directories(base_path() . '/Modules');
+        $universalBundleModulePath = base_path() . '/Modules/UniversalBundle/Modules';
 
         foreach ($modules as $module) {
             $moduleName = basename($module);
 
-                $this->info(' Removing ' . $moduleName . ' from UniversalBundle Modules');
-                echo exec('rm -rf ' . $universalBundleModulePath . '/' . $moduleName);
+            $this->info(' Removing ' . $moduleName . ' from UniversalBundle Modules');
+            echo exec('rm -rf ' . $universalBundleModulePath . '/' . $moduleName);
 
-                // Ignore autotranslate and universalbundle
-                if (in_array(strtolower($moduleName), ['autotranslate', 'universalbundle'])) {
-                    continue;
-                }
+            // Ignore all modules having ignore_in_universal file
+            if (file_exists($module . '/ignore_in_universal')) {
+                continue;
+            }
 
-                $this->info(' Copying ' . $moduleName . ' to UniversalBundle Modules');
-                echo exec('rsync -av --progress ' . $module . ' ' . $universalBundleModulePath . ' --exclude=".git" --exclude=".phpintel" --exclude=".env" --exclude=".idea" --exclude="upload.sh" --exclude=".gitlab-ci.yml" --exclude="Documentation/"');
+            $this->info(' Copying ' . $moduleName . ' to UniversalBundle Modules');
+            echo exec('rsync -av --progress ' . $module . ' ' . $universalBundleModulePath . ' --exclude=".git" --exclude=".phpintel" --exclude=".env" --exclude=".idea" --exclude="upload.sh" --exclude=".gitlab-ci.yml" --exclude="Documentation/"');
 
-                $this->info(' Removing all the lang file from Resources/lang and copy only eng directory');
-                echo exec('rm -rf ' . $universalBundleModulePath . '/' . $moduleName . '/Resources/lang/*');
-                echo exec('cp -r ' . $module . '/Resources/lang/eng ' . $universalBundleModulePath . '/' . $moduleName . '/Resources/lang/');
+            $this->info(' Removing all the lang file from Resources/lang and copy only eng directory');
+            echo exec('rm -rf ' . $universalBundleModulePath . '/' . $moduleName . '/Resources/lang/*');
+            echo exec('cp -r ' . $module . '/Resources/lang/eng ' . $universalBundleModulePath . '/' . $moduleName . '/Resources/lang/');
         }
     }
 
