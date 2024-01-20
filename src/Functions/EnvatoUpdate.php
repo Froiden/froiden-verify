fdea025e97a18cb098c144ba2889c0bd3246cf86
Ajay Kumar Choudhary <ajay@froiden.com>
2024-01-20 15:02:20 +0530

    Update EnvatoUpdate.php

--- src/Functions/EnvatoUpdate.php
+++ src/Functions/EnvatoUpdate.php
@@ -133,54 +133,29 @@ class EnvatoUpdate
     {
         // Verify purchase
 
-        //OLD
-//        try {
-//            $ch = curl_init();
-//            curl_setopt($ch, CURLOPT_URL, config('froiden_envato.verify_url'));
-//
-//            curl_setopt($ch, CURLOPT_POST, 1);
-//            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
-//            // Object Object Error for verification
-//            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-//            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//
-//            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//            $server_output = curl_exec($ch);
-//            $response = json_decode($server_output, true);
-//            curl_close($ch);
-//
-//            return $response;
-//        } catch (\Exception $e) {
-//
-//            return [
-//                'status' => 'success',
-//                'messages' => 'Your purchase code is successfully verified'
-//            ];
-//        }
+       try {
+           $ch = curl_init();
+           curl_setopt($ch, CURLOPT_URL, config('froiden_envato.verify_url'));
 
+           curl_setopt($ch, CURLOPT_POST, 1);
+           curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+           // Object Object Error for verification
+           curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        try {
-            $client = new \GuzzleHttp\Client();
-            $response = $client->post(config('froiden_envato.verify_url'), [
-                'form_params' => $postData,
-                'verify' => false, // Disable SSL verification (use with caution)
-            ]);
+           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+           $server_output = curl_exec($ch);
+           $response = json_decode($server_output, true);
+           curl_close($ch);
 
-            $body = $response->getBody();
+           return $response;
+       } catch (\Exception $e) {
 
-            return json_decode($body, true);
-
-        } catch (\Exception $e) {
-            return [
-                'status' => 'success',
-                'messages' => 'Your purchase code is successfully verified' . $e->getMessage()
-            ];
-        } catch (GuzzleException $e) {
-            return [
-                'status' => 'success',
-                'messages' => 'Your purchase code is successfully verified' . $e->getMessage()
-            ];
-        }
+           return [
+               'status' => 'success',
+               'messages' => 'Your purchase code is successfully verified'. $e->getMessage()
+           ];
+       }
     }
 
 }
