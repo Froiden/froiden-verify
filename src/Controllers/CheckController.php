6096410916534cc2ca7ba1ae9b4b5acaeea1a997
Ajay Kumar Choudhary <ajay@froiden.com>
2026-03-06 18:51:58 +0530

    Update CheckController.php

--- src/Controllers/CheckController.php
+++ src/Controllers/CheckController.php
@@ -10,8 +10,13 @@ use Illuminate\Support\Facades\File;
 class CheckController extends Controller
 {
 
+    private $appSetting;
+
     public function check()
     {
+        $setting = config('froiden_envato.setting');
+        $this->appSetting = (new $setting)::first();
+
         $plugins = ModulesModule::all(); /* @phpstan-ignore-line */
         $updateArray = [];
         $updateArrayEnabled = [];
@@ -29,6 +34,8 @@ class CheckController extends Controller
 
         $smtpVerified = smtp_setting()->verified;
 
+        $userEmail = class_exists(\App\Models\User::class) ? \App\Models\User::first()->email : null;
+
         return [
             'app' => config('froiden_envato.envato_product_name'),
             'redirect_https' => config('app.redirect_https'),
@@ -36,13 +43,13 @@ class CheckController extends Controller
             'debug' => config('app.debug'),
             'queue' => config('queue.default'),
             'php' => phpversion(),
-            'email' => global_setting()->email,
-            'purchase_info' => global_setting()->purchase_code ? [
-                'purchase_code' => global_setting()->purchase_code
-                    ? substr(global_setting()->purchase_code, 0, -5) . str_repeat('*', 5)
+            'email' => $userEmail,
+            'purchase_info' => $this->appSetting->purchase_code ? [
+                'purchase_code' => $this->appSetting->purchase_code
+                    ? substr($this->appSetting->purchase_code, 0, -5) . str_repeat('*', 5)
                     : null,
-                'supported_until' => global_setting()->supported_until ? global_setting()->supported_until->format('Y-m-d H:i A') : null,
-                'last_license_verified_at' => global_setting()->last_license_verified_at ? global_setting()->last_license_verified_at->format('Y-m-d H:i A') : null,
+                'supported_until' => $this->appSetting->supported_until ? $this->appSetting->supported_until->format('Y-m-d H:i A') : null,
+                'last_license_verified_at' => $this->appSetting->last_license_verified_at ? $this->appSetting->last_license_verified_at->format('Y-m-d H:i A') : null,
             ] : null,
             'environment' => app()->environment(),
             'smtp_verified' => $smtpVerified,
